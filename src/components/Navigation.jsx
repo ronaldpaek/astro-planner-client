ad47ca200e7905045c52e3a809b4788affb27c93
Renata Reinartz <rcreinartz@gmail.com>
2023-10-06 21:57:46 -0600

    fixed navbar icon

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -13,6 +13,7 @@ import {
   FaBlog,
   FaDollarSign,
   FaQuestion,
+  FaRegBell,
 } from 'react-icons/fa';
 import logoIcon from '@/assets/moon-logo-v2.svg';
 
@@ -24,87 +25,89 @@ const Navigation = () => {
   };
   return (
     <nav className="desktop-navigation">
-      <ul className="navbar-list">
-        <div className="flex">
-          <Link to="/" className="flex">
-            <li className="navbar-li">
-              <img
-                className="logo-icon"
-                src={logoIcon}
-                size={24}
-                alt="airplane-logo"
-              />
-            </li>
-            <li style={{ fontWeight: 'bold' }} className="navbar-li">
-              Astro Planner
-            </li>
-          </Link>
-        </div>
+      <div className="container">
+        <ul className="navbar-list">
+          <div className="navbar-list-fullwidth">
+            <Link to="/" className="flex">
+              <li className="navbar-list-fullwidth">
+                <img
+                  className="logo-icon"
+                  src={logoIcon}
+                  size={24}
+                  alt="airplane-logo"
+                />
+              </li>
+              <li style={{ fontWeight: 'bold' }} className="logo-type hide ">
+                Astro Planner
+              </li>
+            </Link>
+          </div>
 
-        <div className="navbar-list-fullwidth">
-          <li className="navbar-li">
-            <Link to={'/'}>
-              <FaHome className="icon" />
-              <span className="text home">Home</span>
-            </Link>
-          </li>
-          <li className="navbar-li">
-            <Link to={'/things-to-do'}>
-              <FaBriefcase className="icon" />
-              <span className="text">Things To Do</span>
-            </Link>
-          </li>
-          <li className="navbar-li">
-            <Link to={'/tour'}>
-              <FaPlane className="icon" />
-              <span className="text">Tour</span>
-            </Link>
-          </li>
-          <li className="navbar-li">
-            <Link to={'/blog'}>
-              <FaBlog className="icon" />
-              <span className="text">Blog</span>
-            </Link>
-          </li>
-        </div>
-        <div className="navbar-list-fullwidth">
-          <li className="navbar-li">
-            <Link to={'/usd'}>
-              <FaDollarSign className="icon" />
-              <span className="text">USD</span>
-            </Link>
-          </li>
-          <li className="navbar-li">
-            <Link to={'/faqs'}>
-              <FaQuestion className="icon" />
-              <span className="text">FAQ</span>
-            </Link>
-          </li>
-          <li className="navbar-li">
-            <Link to={'/notifications'}>
-              <img src={notificationIcon} alt="notifications" />
-            </Link>
-          </li>
-          <li className="navbar-li">
-            <img src={userProfilePhoto} alt="user profile photo" />
-          </li>
-        </div>
-        <div className="flex">
-          <DarkModeToggle />
-          <AiOutlineMenu
-            className="menu-icon"
-            size={30}
-            onClick={handleShowNavbar}
-          />
-        </div>
-        <div className="flex">
-          <AiOutlineMenu
-            className="menu-icon"
-            size={30}
-            onClick={handleShowNavbar}
-          />
-        </div>
-      </ul>
+          <div className="navbar-list-fullwidth">
+            <li className="navbar-li">
+              <Link className="flex" to={'/'}>
+                <FaHome className="icon" />
+                <span className="text home">Home</span>
+              </Link>
+            </li>
+            <li className="navbar-li">
+              <Link className="flex" to={'/things-to-do'}>
+                <FaBriefcase className="icon" />
+                <span className="text">To Do</span>
+              </Link>
+            </li>
+            <li className="navbar-li">
+              <Link className="flex" to={'/tour'}>
+                <FaPlane className="icon" />
+                <span className="text">Tour</span>
+              </Link>
+            </li>
+            <li className="navbar-li">
+              <Link className="flex" to={'/blog'}>
+                <FaBlog className="icon" />
+                <span className="text">Blog</span>
+              </Link>
+            </li>
+          </div>
+          <div className="navbar-list-fullwidth">
+            <li className="navbar-li">
+              <Link className="flex" to={'/usd'}>
+                <FaDollarSign className="icon" />
+                <span className="text">USD</span>
+              </Link>
+            </li>
+            <li className="navbar-li">
+              <Link className="flex" to={'/faqs'}>
+                <FaQuestion className="icon" />
+                <span className="text">FAQ</span>
+              </Link>
+            </li>
+            <li className="navbar-li">
+              <Link className="flex" to={'/notifications'}>
+                <FaRegBell className="icon" />
+              </Link>
+            </li>
+            <li className="navbar-li">
+              <img src={userProfilePhoto} alt="user profile photo" />
+            </li>
+            <div className="flex">
+              <DarkModeToggle />
+              <AiOutlineMenu
+                className="menu-icon"
+                size={30}
+                onClick={handleShowNavbar}
+              />
+            </div>
+            <div className="flex">
+              <AiOutlineMenu
+                className="menu-icon"
+                size={30}
+                onClick={handleShowNavbar}
+              />
+            </div>
+          </div>
+        </ul>
+      </div>
     </nav>
   );
 };
