96d7725d4d9859f83a379f3e6c43ed43921cc2da
Renata Reinartz <rcreinartz@gmail.com>
2023-10-08 02:05:39 -0600

    added-hero

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '@/components/Header';
 import TwoWeekTripPlan from '@/components/TwoWeekTripPlan';
 import TripPlanning from '@/components/TripPlanning';
 
 const Home = () => {
   return (
     <div>
+      <Header />
       <TripPlanning />
       <TwoWeekTripPlan />
     </div>
