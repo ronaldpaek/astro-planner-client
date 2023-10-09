2b9a45745a553c47607310bb13eb75b266d7170a
itsmonicabena <87464371+itsmonicabena@users.noreply.github.com>
2023-10-08 23:26:15 -0500

    Easy Trip Booking component

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Header from '@/components/Header';
 import TwoWeekTripPlan from '@/components/TwoWeekTripPlan';
 import TripPlanning from '@/components/TripPlanning';
-import DiscoverFeatures from '../components/DiscoverFeatures';
+import DiscoverFeatures from '@/components/DiscoverFeatures';
+import EasyTripBooking from '@/components/EasyTripBooking';
 
 const Home = () => {
   return (
     <div>
       <Header />
       <TripPlanning />
+      <EasyTripBooking />
       <DiscoverFeatures />
       <TwoWeekTripPlan />
     </div>
